d74c0a3635a94075d3164dda21cdee017c16df9d
Germán Cutraro <germancutraro@hotmail.com>
2017-10-13 17:19:54 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const app = express();
 // Middlewares
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use('/public', express.static('public'));
 
 const {port, engine} = require('./config'); //es6 destructuring
